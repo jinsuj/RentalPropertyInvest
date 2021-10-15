f30ab1322ab139e0ec177edd79eea0812abb8b17
jinsuj <61698878+jinsuj@users.noreply.github.com>
2021-10-14 23:13:51 -0500

    Updating the folder location

--- src/test/java/JDBCConnector/DBConnectionTest.java
+++ src/test/java/JDBCConnector/DBConnectionTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 class DBConnectionTest {
 
     DBConnection conn = new DBConnection();
